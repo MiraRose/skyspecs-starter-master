c483162ec3d8ad50c343d27f81ec00e1363e8520
MiraRose <miraastar@gmail.com>
2022-02-17 15:40:13 -0500

    Put in initial changes toward adding to web app

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,31 @@ import React, { useEffect, useState } from "react";
 import logo from "./SS.svg";
 import "./App.css";
 
+function fetchAPI(userName) {
+  return fetch(`http://localhost:3010/users/${userName}`)
+}
+
 const App = () => {
   const [connected, setConnected] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [gists, setGists] = useState([])
+
+  function handleOnClick() {
+    fetchAPI(userName)
+    .then(response => response.json())
+    .then((result) => {
+      setGists(result)
+    })
+  }
+
+  function renderGists() {
+    return (
+      <div>
+        {gists.map(gist => <div key={gist.id}></div>)}
+      </div>
+    )
+  }
+
   useEffect(() => {
     const timer = setInterval(
       async () =>
@@ -30,6 +53,13 @@ const App = () => {
         </a>
         <p>Your Server is {connected ? "" : "not "}running</p>
       </header>
+
+      <div>
+        <label>Github Username to get Gists for: </label>
+        <input type="text" onChange={ (event) => setUserName(event.target.value) } value={userName}></input>
+        <button onClick={() => handleOnClick()}>Get Gists</button>
+      </div>
+      <div>{renderGists()}</div>
     </div>
   );
 };
