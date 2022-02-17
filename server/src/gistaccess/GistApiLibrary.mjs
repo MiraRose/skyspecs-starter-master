52dc3d767004d37b63b7bddaaca2d04554eb078c
MiraRose <miraastar@gmail.com>
2022-02-17 15:18:53 -0500

    Some cleanup of endpoint and such

--- server/src/gistaccess/GistApiLibrary.mjs
+++ server/src/gistaccess/GistApiLibrary.mjs
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import fetch from "node-fetch";
 
 export async function getGistsForUser(req, res) {
     axios.get('https://api.github.com/users/' + req.params.userName + '/gists')
@@ -7,7 +6,7 @@ export async function getGistsForUser(req, res) {
      .catch(err => res.send(err));
 }
 
-export function getGistByID(req, res) {
+export async function getGistByID(req, res) {
     axios.get('https://api.github.com/gists/' + req.params.gistID)
      .then(response => res.json(response.data))
      .catch(err => res.send(err));
--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,9 @@ import axios from 'axios'
 
 const PORT = 3010;
 
-import { getGistByID, getGistsForUser, retrieveGistsForUser } from './gistaccess/GistApiLibrary.mjs';
+import { getGistByID, getGistsForUser } from './gistaccess/GistApiLibrary.mjs';
 
 const app = express();
-const router = express.Router()
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -18,8 +17,8 @@ app.get("/users/:userName", (req, res) => {
   getGistsForUser(req, res)
 });
 
-app.get("/users/:gistID", (req, res) => {
-  getGistsForUser(req, res)
+app.get("/gists/:gistID", (req, res) => {
+  getGistByID(req, res)
 });
 
 app.listen(PORT, () => {
