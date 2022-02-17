c0907888fc629c3d968241fa6b4c034cbe421277
MiraRose <miraastar@gmail.com>
2022-02-17 13:57:31 -0500

    Added basic axios calls to library

--- server/src/gistaccess/GistApiLibrary.js
+++ server/src/gistaccess/GistApiLibrary.js
@@ -1,7 +1,11 @@
-export function getGistsForUser() {
+import 'axios'
 
+export function getGistsForUser(username) {
+    let response = axios('https://api.github.com/users/' + username + '/gists');
+    return response.data
 }
 
-export function getGistByID() {
-    
+export function getGistByID(id) {
+    let response = axios('https://api.github.com/gists/' + id);
+    return response.data
 }
