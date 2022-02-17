13108c1d67517a16fcef0c280e2141fa443d4045
MiraRose <miraastar@gmail.com>
2022-02-17 14:20:29 -0500

    Changes to make library work as module Added endpoint for getting by user

---
+++ server/src/gistaccess/GistApiLibrary.mjs
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export function getGistsForUser(username) {
+    let response = axios('https://api.github.com/users/' + username + '/gists');
+    return response.data
+}
+
+export function getGistByID(id) {
+    let response = axios('https://api.github.com/gists/' + id);
+    return response.data
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,19 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
 const PORT = 3010;
+
+import { getGistByID, getGistsForUser } from './gistaccess/GistApiLibrary.mjs';
 
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to SkySpecs!");
+});
+
+app.get("/users/:userName", (req, res) => {
+  let gists = getGistsForUser(req.params.userName)
+  res.send(gists);
 });
 
 app.listen(PORT, () => {
